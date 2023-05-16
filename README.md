dd58b4b8a866ac148c7d28e68a82bb2fe4efcc57
kmurphy61 <58490237+kmurphy61@users.noreply.github.com>
2023-05-16 12:11:17 -0500

    update files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# plantcv-v4-tutorial-VIS-single-plant
-PlantCV Tutorial using release-4.0 for single plant, VIS image (RGB) analysis
+# PlantCV Single Plant, VIS Image Analysis Tutorial using release-4.0
+
+An introduction to analyzing VIS (RGB) images of single plants (or single objects) with PlantCV.
+
+# Image Analysis Workflow
+
+Read in image taken with any kind of vis (RGB) camera to analyze shape, color, size, and more properties. 
+
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/danforthcenter/plantcv-stomata-tutorial/HEAD)
+
+## Tutorial tags/keywords
+
+RGB, VIS, tutorial, shape, color, plant, single
+
+## Citations
+
+Optional.
---
+++ tests.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+import pytest
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+TEST_TMPDIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".cache")
+
+
+# ##########################
+# Tests setup function
+# ##########################
+def setup_function():
+    if not os.path.exists(TEST_TMPDIR):
+        os.mkdir(TEST_TMPDIR)
+
+
+# ##########################
+# Tests executing the notebook
+# ##########################
+def test_notebook():
+    # Open the notebook
+    with open("index.ipynb", "r") as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Process the notebook
+    ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    ep.preprocess(nb, {"metadata": {"path": TEST_TMPDIR}})
+
+    # Save the executed notebook
+    out_nb = os.path.join(TEST_TMPDIR, "executed_notebook.ipynb")
+    with open(out_nb, "w", encoding="utf-8") as f:
+        nbformat.write(nb, f)
+
+    assert os.path.exists(out_nb)
+
+
+# ##############################
+# Clean up test files
+# ##############################
+def teardown_function():
+    shutil.rmtree(TEST_TMPDIR)
